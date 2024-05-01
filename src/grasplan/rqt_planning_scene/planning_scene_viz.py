c043f6f1483f6dcc119f33ff020e63934c65a9d1
Julian Arkenau <julian.arkenau@dfki.de>
2024-05-01 15:41:40 +0200

    store updated planning scene

--- src/grasplan/rqt_planning_scene/planning_scene_viz.py
+++ src/grasplan/rqt_planning_scene/planning_scene_viz.py
@@ -26,33 +26,42 @@ from marker_tools import make_box, Color, make_int_marker
 from interactive_markers.interactive_marker_server import InteractiveMarkerServer, InteractiveMarkerFeedback
 
 
-def callback(feedback: InteractiveMarkerFeedback) -> None:
-    """
-    Callback function for handling interactive marker feedback.
-
-    Args:
-        feedback (InteractiveMarkerFeedback): The feedback object containing information about the interactive marker.
-
-    Returns:
-        None
-    """
-    if feedback.event_type == InteractiveMarkerFeedback.POSE_UPDATE:
-        print(f"Updated {feedback.marker_name}")
-
-
-class PlanningSceneViz:
-    def __init__(self) -> None:
+# TODO: move to .sence file format from MoveIt!
+class PlanningSceneHelper:
+    def __init__(self, scene_path: str) -> None:
         self.marker_server = InteractiveMarkerServer("interactive_marker")
+        self.scene_path = Path(scene_path)
+        self.data = None
 
-    def publish_boxes(self, yaml_path: str) -> None:
-        yaml_path = Path(yaml_path)
-        if not yaml_path.exists() or not yaml_path.is_file() or yaml_path.suffix != ".yaml":
-            raise FileNotFoundError(f"YAML file not found at: {yaml_path.absolute()}")
-        with open(yaml_path, "r") as file:
-            data = yaml.safe_load(file)
+        self._load_scene(self.scene_path)
 
-        # TODO: move to .sence file format from MoveIt!
-        for box in data["planning_scene_boxes"]:
+    def _load_scene(self, path: Path) -> None:
+        if not path.exists() or not path.is_file() or path.suffix != ".yaml":
+            raise FileNotFoundError(f"YAML file not found at: {path.absolute()}")
+        with open(path, "r") as file:
+            self.data = yaml.safe_load(file)
+
+    def _save_scene(self, path: Path) -> None:
+        if path.suffix != ".yaml":
+            raise ValueError(f"Path must have a '.yaml' ending: {path.absolute()}")
+        with open(path, "w") as f:
+            yaml.dump(self.data, f)
+
+    def _update_scene(self, feedback: InteractiveMarkerFeedback) -> None:
+        if feedback.event_type == InteractiveMarkerFeedback.POSE_UPDATE:
+            for box in self.data["planning_scene_boxes"]:
+                if box["scene_name"] == feedback.marker_name:
+                    box["box_orientation_w"] = feedback.pose.orientation.w
+                    box["box_orientation_x"] = feedback.pose.orientation.x
+                    box["box_orientation_y"] = feedback.pose.orientation.y
+                    box["box_orientation_z"] = feedback.pose.orientation.z
+                    box["box_position_x"] = feedback.pose.position.x
+                    box["box_position_y"] = feedback.pose.position.y
+                    box["box_position_z"] = feedback.pose.position.z
+        self._save_scene(self.scene_path)
+
+    def publish_boxes(self) -> None:
+        for box in self.data["planning_scene_boxes"]:
             pose = Pose(
                 position=Point(x=box["box_position_x"], y=box["box_position_y"], z=box["box_position_z"]),
                 orientation=Quaternion(
@@ -65,13 +74,13 @@ class PlanningSceneViz:
             scale = Vector3(x=box["box_x_dimension"], y=box["box_y_dimension"], z=box["box_z_dimension"])
             marker = make_box(pose, Color.GREEN, scale)
             int_marker = make_int_marker(box["scene_name"], pose, marker)
-            self.marker_server.insert(int_marker, callback)
+            self.marker_server.insert(int_marker, self._update_scene)
 
         self.marker_server.applyChanges()
 
 
 if __name__ == "__main__":
     rospy.init_node("planning_scene_viz")
-    viz = PlanningSceneViz()
-    viz.publish_boxes("config/examples/planning_scene.yaml")
+    viz = PlanningSceneHelper("config/examples/planning_scene.yaml")
+    viz.publish_boxes()
     rospy.spin()
